6f6084fb745eb0c38ea3ed45844d2d9af6c56b47
PanMan <martijn@panman.nl>
2014-10-23 16:53:11 +0200

    plugin.php created

---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Add Advance Custom Fields to JSON API
+ * Description: Add Advance Custom Fields to JSON API - from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51
+ * Author: @PanMan
+ * Author URI: https://github.com/PanManAms/WP-JSON-API-ACF
+ * Version: 0.1
+ * Plugin URI: https://github.com/PanManAms/WP-JSON-API-ACF
+ * Copied from https://gist.github.com/rileypaulsen/9b4505cdd0ac88d5ef51 - but a plugin is nicer
+ */
+function wp_api_encode_acf($data,$post,$context){
+    $customMeta = (array) get_fields($post['ID']);
+
+    $data['meta'] = array_merge($data['meta'], $customMeta );
+    return $data;
+}
+
+if( function_exists('get_fields') ){
+    add_filter('json_prepare_post', 'wp_api_encode_acf', 10, 3);
+}
